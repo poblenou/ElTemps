2f41563a192c6c8886667574fcb7a74735883437
carlesgm <carlesgm@gmail.com>
2015-11-01 23:01:40 +0100

    Completem el WeatherAdapter

--- app/src/main/java/com/example/poblenou/eltemps/WeatherAdapter.java
+++ app/src/main/java/com/example/poblenou/eltemps/WeatherAdapter.java
@@ -1,12 +1,38 @@
 package com.example.poblenou.eltemps;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import com.example.poblenou.eltemps.json.List;
 
 public class WeatherAdapter extends ArrayAdapter<List> {
     public WeatherAdapter(Context context, int resource, java.util.List<List> objects) {
         super(context, resource, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Obtenim l'objecte en la posició corresponent
+        List item = getItem(position);
+
+        // Mirem a veure si la View s'està reusant, si no es així "inflem" la View
+        // https://github.com/codepath/android_guides/wiki/Using-an-ArrayAdapter-with-ListView#row-view-recycling
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.listview_forecasts_row, parent, false);
+        }
+
+        // Unim el codi en les Views del Layout
+        TextView tvForecast = (TextView) convertView.findViewById(R.id.tvForecast);
+
+        // Fiquem les dades dels objectes (provinents del JSON) en el layout
+        tvForecast.setText(item.getForecastString());
+
+        // Retornem la View replena per a mostrarla
+        return convertView;
     }
 }
