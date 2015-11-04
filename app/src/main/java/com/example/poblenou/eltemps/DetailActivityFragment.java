d9a0260e7d4f97732c28c48d1824a7a3f5ef9ca4
carlesgm <carlesgm@gmail.com>
2015-11-05 00:27:22 +0100

    Fiquem les dades en el detail fragment

--- app/src/main/java/com/example/poblenou/eltemps/DetailActivityFragment.java
+++ app/src/main/java/com/example/poblenou/eltemps/DetailActivityFragment.java
@@ -1,11 +1,14 @@
 package com.example.poblenou.eltemps;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.poblenou.eltemps.json.List;
@@ -15,20 +18,48 @@ import com.example.poblenou.eltemps.json.List;
  */
 public class DetailActivityFragment extends Fragment {
 
+    private ImageView mIconView;
+    private TextView mDateView;
+    private TextView mFriendlyDateView;
+    private TextView mDescriptionView;
+    private TextView mHighTempView;
+    private TextView mLowTempView;
+    private TextView mHumidityView;
+    private TextView mWindView;
+    private TextView mPressureView;
+
     public DetailActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
 
         Intent i = getActivity().getIntent();
         List item = (List) i.getSerializableExtra("item");
 
-        TextView tvForecast = (TextView) view.findViewById(R.id.tvForecast);
-        tvForecast.setText(item.getForecastString());
+        mIconView = (ImageView) rootView.findViewById(R.id.detail_icon);
+        mDateView = (TextView) rootView.findViewById(R.id.detail_date_textview);
+        mFriendlyDateView = (TextView) rootView.findViewById(R.id.detail_day_textview);
+        mDescriptionView = (TextView) rootView.findViewById(R.id.detail_forecast_textview);
+        mHighTempView = (TextView) rootView.findViewById(R.id.detail_high_textview);
+        mLowTempView = (TextView) rootView.findViewById(R.id.detail_low_textview);
+        mHumidityView = (TextView) rootView.findViewById(R.id.detail_humidity_textview);
+        mWindView = (TextView) rootView.findViewById(R.id.detail_wind_textview);
+        mPressureView = (TextView) rootView.findViewById(R.id.detail_pressure_textview);
 
-        return view;
+        mFriendlyDateView.setText(item.getFormattedTemp());
+        mDescriptionView.setText(item.getWeather().get(0).getDescription());
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+        String units = preferences.getString("units", "metric");
+        mHighTempView.setText(item.getMaxTemp(units));
+        mLowTempView.setText(item.getMinTemp(units));
+
+        mHumidityView.setText(item.getHumidity().toString());
+        mWindView.setText(item.getSpeed().toString());
+        mPressureView.setText(item.getPressure().toString());
+        return rootView;
     }
 }
